9c434897525fdc7201f841a634a2ccb7fd095c81
김대영 <rlaeo28@gmail.com>
2023-04-05 10:50:12 +0900

    0405 커밋 테스트

--- MyB_Project/src/main/java/com/project/myapp/myb/admin/AdminController.java
+++ MyB_Project/src/main/java/com/project/myapp/myb/admin/AdminController.java
@@ -25,7 +25,9 @@ public class AdminController {
 	
 	@Autowired
 	IKindergartenService kindergartenService;
-
+	
+	// 커밋테스트
+	
 	// 로그인 페이지 이동
 	@RequestMapping(value="/login", method=RequestMethod.GET)
 	public String login() {
